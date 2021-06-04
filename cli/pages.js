edcaecd3bd6bb5cbb733e722cdf01d1e1888a716
alexsan-dev <alexdsantosv@gmail.com>
2021-06-04 15:40:26 -0600

    [ Add ] add page script in node

---
+++ cli/pages.js
@@ -0,0 +1,62 @@
+// FILE SYSTEM
+const path = require('path')
+const fs = require('fs')
+
+// ARGUMENTOS
+const args = process.argv
+const pageName = args[2]
+
+// ESCRIBIR ARCHIVO
+const writeFile = (dirName, ext, content) => {
+	fs.writeFile(
+		path.resolve(`./${dirName}/pages/${pageName}/index.${ext}`),
+		content,
+		'utf-8',
+		(err) => {
+			if (err) console.log(err)
+		},
+	)
+}
+
+// ESCRIBIR CARPETA
+const createDir = (dirName, cb) => {
+	fs.mkdir(
+		path.resolve(`./${dirName}/pages/${pageName}`),
+		{ recursive: true },
+		(err) => {
+			if (err) console.log(err)
+			else cb()
+		},
+	)
+}
+
+// CREAR PAGINA EN PUBLIC
+fs.exists(path.resolve(`./public/pages/${pageName}/index.html`), (exists) => {
+	if (!exists) {
+		// ESCRIBIR CARPETA
+		createDir('public', () => {
+			writeFile(
+				'public',
+				'html',
+				`<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>${pageName}</title>
+    </head>
+    <body>
+        <script src="../../../scripts/pages/${pageName}/index.js"></script>
+    </body>
+</html>
+            `,
+			)
+		})
+
+		// ESCRIBIR TYPESCRIPT
+		createDir('lib', () => {
+			writeFile('lib', 'ts', '')
+		})
+	} else console.log('Esta pagina ya existe')
+})
