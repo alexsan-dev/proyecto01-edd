e61bb4519e25bdd5726d14836911682ad64f2904
rl102306 <2306919780101@ingenieria.usac.edu.gt>
2021-07-04 14:02:18 -0600

    [Costo Uniforme] ADD
    Algoritmo costo uniforme

---
+++ lib/pages/costouniforme/index.ts
@@ -0,0 +1,36 @@
+/* 
+
+Python Code
+
+def costo_uniforme (nodo_inicio, nodo_fin):
+
+    lista = [[nodo_inicio,0]]
+
+    while lista:
+
+        nodo_actual = lista.pop(0)
+
+        print(nodo_acutal)
+
+        if nodo_actual[0] == nodo_fin[0]
+
+            return print("Solucion")
+
+        temp = sucesores (nodo_actual)
+
+        print(temp)
+
+        if temp:
+
+            lista.extend(temp)
+
+            lista.sort(key=lambda x: int(x[1]))
+
+            print(lista)
+        
+    print("No solucion")
+
+
+
+
+*/
